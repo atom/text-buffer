e1fbd5cd097cf11d9378025c439c35b8412651c8
Ben Ogle <ogle.ben@gmail.com>
2014-09-10 14:57:19 -0700

    :lipstick:

--- script/prepublish.js
+++ script/prepublish.js
@@ -6,7 +6,7 @@ var updateCommand = 'npm update grunt-atomdoc'
 var docCommand = 'grunt clean lint coffee atomdoc'
 child_process.exec(updateCommand, options, function(error, stdout, stderr) {
   if(stderr) console.error(stderr);
-  child_process.exec(docCommand, options, function(error, stdout, stderr){
+  child_process.exec(docCommand, options, function(error, stdout, stderr) {
     if(stderr) console.error(stderr);
   })
 })
