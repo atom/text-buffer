a345d596f4672f723fc7ada1fd4bb017a4b91916
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-04-27 12:15:24 -0600

    Use MarkerIndex.findBoundariesAfter in TestDecorationLayer

--- spec/helpers/test-decoration-layer.js
+++ spec/helpers/test-decoration-layer.js
@@ -1,6 +1,5 @@
 const {MarkerIndex} = require('superstring')
 const {Emitter} = require('event-kit')
-const {compare: comparePoints, isEqual: isEqualPoint, min: minPoint} = require('../../src/point-helpers')
 const Point = require('../../src/point')
 const Range = require('../../src/range')
 const {MAX_BUILT_IN_SCOPE_ID} = require('../../src/constants')
@@ -10,7 +9,6 @@ class TestDecorationLayer {
   constructor (decorations, buffer, random) {
     this.buffer = buffer
     this.random = random
-    // TODO: Set this back to 1 when we introduce the composite decoration layer
     this.nextMarkerId = MAX_BUILT_IN_SCOPE_ID + 1
     this.markerIndex = new MarkerIndex()
     this.classNamesByScopeId = new Map()
@@ -106,92 +104,13 @@ class TestDecorationLayer {
 class TestDecorationLayerIterator {
   constructor (layer) {
     this.layer = layer
-    const {markerIndex, classNamesByScopeId} = this.layer
-
-    const emptyMarkers = []
-    const nonEmptyMarkers = []
-    for (let key of classNamesByScopeId.keys()) {
-      const id = parseInt(key)
-      if (isEqualPoint(markerIndex.getStart(id), markerIndex.getEnd(id))) {
-        emptyMarkers.push(id)
-      } else {
-        nonEmptyMarkers.push(id)
-      }
-    }
-
-    emptyMarkers.sort((a, b) => comparePoints(markerIndex.getStart(a), markerIndex.getStart(b)) || (a - b))
-
-    const markersSortedByStart = nonEmptyMarkers.slice().sort((a, b) => comparePoints(markerIndex.getStart(a), markerIndex.getStart(b)) || (a - b))
-
-    const markersSortedByEnd = nonEmptyMarkers.slice().sort((a, b) => comparePoints(markerIndex.getEnd(a), markerIndex.getEnd(b)) || (b - a))
-
-    this.boundaries = []
-
-    const nextEmptyMarkerStart = () => (emptyMarkers.length > 0) && markerIndex.getStart(emptyMarkers[0])
-    const nextMarkerStart = () => (markersSortedByStart.length > 0) && markerIndex.getStart(markersSortedByStart[0])
-    const nextMarkerEnd = () => (markersSortedByEnd.length > 0) && markerIndex.getEnd(markersSortedByEnd[0])
-
-    while ((emptyMarkers.length > 0) || (markersSortedByStart.length > 0) || (markersSortedByEnd.length > 0)) {
-      let boundary = {
-        position: Point.INFINITY,
-        closeScopeIds: [],
-        openScopeIds: []
-      }
-
-      if (nextMarkerStart()) {
-        boundary.position = minPoint(boundary.position, nextMarkerStart())
-      }
-      if (nextEmptyMarkerStart()) {
-        boundary.position = minPoint(boundary.position, nextEmptyMarkerStart())
-      }
-      if (nextMarkerEnd()) {
-        boundary.position = minPoint(boundary.position, nextMarkerEnd())
-      }
-
-      while (nextMarkerEnd() && isEqualPoint(nextMarkerEnd(), boundary.position)) {
-        boundary.closeScopeIds.push(markersSortedByEnd.shift())
-      }
-
-      const emptyScopeIds = []
-      while (nextEmptyMarkerStart() && isEqualPoint(nextEmptyMarkerStart(), boundary.position)) {
-        emptyScopeIds.push(emptyMarkers.shift())
-      }
-
-      if (emptyScopeIds.length > 0) {
-        boundary.openScopeIds.push(...emptyScopeIds)
-        this.boundaries.push(boundary)
-        boundary = {
-          position: boundary.position,
-          closeScopeIds: [],
-          openScopeIds: []
-        }
-        boundary.closeScopeIds.push(...emptyScopeIds)
-      }
-
-      while (nextMarkerStart() && isEqualPoint(nextMarkerStart(), boundary.position)) {
-        boundary.openScopeIds.push(markersSortedByStart.shift())
-      }
-
-      this.boundaries.push(boundary)
-    }
   }
 
   seek (position) {
-    const containingScopeIds = []
-    for (let index = 0; index < this.boundaries.length; index++) {
-      const boundary = this.boundaries[index]
-      if (comparePoints(boundary.position, position) >= 0) {
-        this.boundaryIndex = index
-        return containingScopeIds
-      } else {
-        for (let scopeId of boundary.closeScopeIds) {
-          containingScopeIds.splice(containingScopeIds.lastIndexOf(scopeId), 1)
-        }
-        containingScopeIds.push(...boundary.openScopeIds)
-      }
-    }
-    this.boundaryIndex = this.boundaries.length
-    return containingScopeIds
+    const {containingStart, boundaries} = this.layer.markerIndex.findBoundariesAfter(position, Number.MAX_SAFE_INTEGER)
+    this.boundaries = boundaries
+    this.boundaryIndex = 0
+    return containingStart
   }
 
   moveToSuccessor () {
@@ -204,12 +123,28 @@ class TestDecorationLayerIterator {
   }
 
   getCloseScopeIds () {
+    const result = []
     const boundary = this.boundaries[this.boundaryIndex]
-    return boundary ? boundary.closeScopeIds : []
+    if (boundary) {
+      boundary.ending.forEach((markerId) => {
+        if (!boundary.starting.has(markerId)) {
+          result.push(markerId)
+        }
+      })
+    }
+    return result
   }
 
   getOpenScopeIds () {
+    const result = []
     const boundary = this.boundaries[this.boundaryIndex]
-    return boundary ? boundary.openScopeIds : []
+    if (boundary) {
+      boundary.starting.forEach((markerId) => {
+        if (!boundary.ending.has(markerId)) {
+          result.push(markerId)
+        }
+      })
+    }
+    return result
   }
 }
