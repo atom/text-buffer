8ea916f526083c4717519a1b23c2f5bf0eb0eec2
Antonio Scandurra <as-cii@github.com>
2017-10-05 16:01:04 +0200

    Fix buffer coordinate translation for soft-wrapped atomic soft tabs

--- spec/display-layer-spec.js
+++ spec/display-layer-spec.js
@@ -883,46 +883,60 @@ describe('DisplayLayer', () => {
     })
 
     it('translates points correctly on soft-wrapped lines', () => {
-      const buffer = new TextBuffer({
-        text: '   abc defgh'
-      })
+      {
+        const buffer = new TextBuffer({text: '   abc defgh'})
+        const displayLayer = buffer.addDisplayLayer({
+          softWrapColumn: 8,
+          softWrapHangingIndent: 2
+        })
 
-      const displayLayer = buffer.addDisplayLayer({
-        softWrapColumn: 8,
-        softWrapHangingIndent: 2
-      })
+        expect(displayLayer.getText()).toBe('   abc \n     def\n     gh')
+        expectPositionTranslations(displayLayer, [
+          [Point(0, 0), Point(0, 0)],
+          [Point(0, 1), Point(0, 1)],
+          [Point(0, 2), Point(0, 2)],
+          [Point(0, 3), Point(0, 3)],
+          [Point(0, 4), Point(0, 4)],
+          [Point(0, 5), Point(0, 5)],
+          [Point(0, 6), Point(0, 6)],
+          [Point(0, 7), [Point(0, 6), Point(0, 7)]],
+          [Point(0, 8), [Point(0, 6), Point(0, 7)]],
+          [Point(1, 0), [Point(0, 6), Point(0, 7)]],
+          [Point(1, 1), [Point(0, 6), Point(0, 7)]],
+          [Point(1, 2), [Point(0, 6), Point(0, 7)]],
+          [Point(1, 3), [Point(0, 6), Point(0, 7)]],
+          [Point(1, 4), [Point(0, 6), Point(0, 7)]],
+          [Point(1, 5), Point(0, 7)],
+          [Point(1, 6), Point(0, 8)],
+          [Point(1, 7), Point(0, 9)],
+          [Point(1, 8), [Point(0, 9), Point(0, 10)]],
+          [Point(1, 9), [Point(0, 9), Point(0, 10)]],
+          [Point(2, 0), [Point(0, 9), Point(0, 10)]],
+          [Point(2, 1), [Point(0, 9), Point(0, 10)]],
+          [Point(2, 2), [Point(0, 9), Point(0, 10)]],
+          [Point(2, 3), [Point(0, 9), Point(0, 10)]],
+          [Point(2, 4), [Point(0, 9), Point(0, 10)]],
+          [Point(2, 5), Point(0, 10)],
+          [Point(2, 6), Point(0, 11)],
+          [Point(2, 7), Point(0, 12)]
+        ])
+      }
 
-      expect(JSON.stringify(displayLayer.getText())).toBe(JSON.stringify('   abc \n     def\n     gh'))
+      {
+        // Translating in the middle of an atomic soft tab that has been soft-wrapped.
+        const buffer = new TextBuffer({text: '    '})
+        const displayLayer = buffer.addDisplayLayer({tabLength: 2, softWrapColumn: 3})
 
-      expectPositionTranslations(displayLayer, [
-        [Point(0, 0), Point(0, 0)],
-        [Point(0, 1), Point(0, 1)],
-        [Point(0, 2), Point(0, 2)],
-        [Point(0, 3), Point(0, 3)],
-        [Point(0, 4), Point(0, 4)],
-        [Point(0, 5), Point(0, 5)],
-        [Point(0, 6), Point(0, 6)],
-        [Point(0, 7), [Point(0, 6), Point(0, 7)]],
-        [Point(0, 8), [Point(0, 6), Point(0, 7)]],
-        [Point(1, 0), [Point(0, 6), Point(0, 7)]],
-        [Point(1, 1), [Point(0, 6), Point(0, 7)]],
-        [Point(1, 2), [Point(0, 6), Point(0, 7)]],
-        [Point(1, 3), [Point(0, 6), Point(0, 7)]],
-        [Point(1, 4), [Point(0, 6), Point(0, 7)]],
-        [Point(1, 5), Point(0, 7)],
-        [Point(1, 6), Point(0, 8)],
-        [Point(1, 7), Point(0, 9)],
-        [Point(1, 8), [Point(0, 9), Point(0, 10)]],
-        [Point(1, 9), [Point(0, 9), Point(0, 10)]],
-        [Point(2, 0), [Point(0, 9), Point(0, 10)]],
-        [Point(2, 1), [Point(0, 9), Point(0, 10)]],
-        [Point(2, 2), [Point(0, 9), Point(0, 10)]],
-        [Point(2, 3), [Point(0, 9), Point(0, 10)]],
-        [Point(2, 4), [Point(0, 9), Point(0, 10)]],
-        [Point(2, 5), Point(0, 10)],
-        [Point(2, 6), Point(0, 11)],
-        [Point(2, 7), Point(0, 12)]
-      ])
+        expect(displayLayer.getText()).toBe('   \n ')
+
+        expect(displayLayer.translateBufferPosition([0, 3], {clipDirection: 'backward'})).toEqual([0, 2])
+        expect(displayLayer.translateBufferPosition([0, 3], {clipDirection: 'closest'})).toEqual([0, 2])
+        expect(displayLayer.translateBufferPosition([0, 3], {clipDirection: 'forward'})).toEqual([1, 1])
+
+        expect(displayLayer.translateScreenPosition([1, 0], {clipDirection: 'backward'})).toEqual([0, 2])
+        expect(displayLayer.translateScreenPosition([1, 0], {clipDirection: 'closest'})).toEqual([0, 2])
+        expect(displayLayer.translateScreenPosition([1, 0], {clipDirection: 'forward'})).toEqual([0, 4])
+      }
     })
 
     it('prefers the skipSoftWrapIndentation option over clipDirection when translating points', () => {
