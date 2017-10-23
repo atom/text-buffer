001f29e58729fb4da439cffc7be18e328bccfa7a
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-10-23 11:59:26 -0700

    Make sure that destroyAllFolds works regardless of fold order

--- spec/display-layer-spec.js
+++ spec/display-layer-spec.js
@@ -429,21 +429,22 @@ describe('DisplayLayer', () => {
 
     it('allows all folds to be destroyed', () => {
       const buffer = new TextBuffer({
-        text: 'abc\ndef\nghi\nj'
+        text: 'abc\ndef\nghi\njkl\nmno'
       })
 
       const displayLayer = buffer.addDisplayLayer()
+      displayLayer.foldBufferRange([[4, 1], [4, 2]])
       displayLayer.foldBufferRange([[0, 1], [1, 2]])
       displayLayer.foldBufferRange([[1, 1], [2, 2]])
       displayLayer.foldBufferRange([[2, 1], [3, 0]])
       displayLayer.foldBufferRange([[2, 2], [3, 0]])
-      expect(displayLayer.getText()).toBe('a⋯j')
+      expect(displayLayer.getText()).toBe('a⋯jkl\nm⋯o')
 
       verifyChangeEvent(displayLayer, () => {
         displayLayer.destroyAllFolds()
       })
 
-      expect(displayLayer.getText()).toBe('abc\ndef\nghi\nj')
+      expect(displayLayer.getText()).toBe('abc\ndef\nghi\njkl\nmno')
     })
 
     it('automatically destroy folds when they become invalid after a buffer change', () => {
