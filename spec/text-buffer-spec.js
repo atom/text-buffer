32ba0691f82b2be6b8d0c9eff140db6c12bfd8ab
Darangi <small_musa@hotmail.com>
2019-11-15 21:09:55 +0100

    fixed lint

--- spec/text-buffer-spec.js
+++ spec/text-buffer-spec.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const TextBuffer = require ('../src/text-buffer');
+const path = require('path')
+const TextBuffer = require('../src/text-buffer')
 
 describe('when a buffer is already open', () => {
-  it('replaces foo( with bar( using /\bfoo\(\b/gim', () => {
-    const filePath = path.join(__dirname, 'fixtures','sample.js');
-    const buffer = new TextBuffer();
-    buffer.setPath(filePath);
-    buffer.setText('foo(x)');
-    buffer.replace(/\bfoo\(\b/gim,'bar(');
+  it('replaces foo( with bar( using /\bfoo\\(\b/gim', () => {
+    const filePath = path.join(__dirname, 'fixtures', 'sample.js')
+    const buffer = new TextBuffer()
+    buffer.setPath(filePath)
+    buffer.setText('foo(x)')
+    buffer.replace(/\bfoo\(\b/gim, 'bar(')
 
-    expect(buffer.getText()).toBe('bar(x)');
-  });
-});
+    expect(buffer.getText()).toBe('bar(x)')
+  })
+})
