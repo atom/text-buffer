a7830322835b18c3e1c80051a439da730358b749
Darangi <small_musa@hotmail.com>
2020-03-17 00:01:52 +0100

    added failing test

--- spec/text-buffer-spec.js
+++ spec/text-buffer-spec.js
@@ -11,4 +11,14 @@ describe('when a buffer is already open', () => {
 
     expect(buffer.getText()).toBe('bar(x)')
   })
+
+  it('replaces tstat_fvars()->curr_setpoint[HEAT_EN] = new_tptr->heat_limit; with tstat_set_curr_setpoint(HEAT_EN, new_tptr->heat_limit);', () => {
+    const filePath = path.join(__dirname, 'fixtures', 'sample.js')
+    const buffer = new TextBuffer()
+    buffer.setPath(filePath)
+    buffer.setText('if (tstat_fvars()->curr_setpoint[HEAT_EN] > new_tptr->heat_limit) { tstat_fvars()->curr_setpoint[HEAT_EN] = new_tptr->heat_limit; }')
+    buffer.replace(/tstat_fvars\(\)->curr_setpoint\[(.+?)\] = (.+?);/, 'tstat_set_curr_setpoint($1, $2);')
+
+    expect(buffer.getText()).toBe('if (tstat_fvars()->curr_setpoint[HEAT_EN] > new_tptr->heat_limit) { tstat_set_curr_setpoint(HEAT_EN, new_tptr->heat_limit); }')
+  })
 })
