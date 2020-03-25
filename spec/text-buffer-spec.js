e8ef49bf09dda4134722c017484b81db9b20173e
Darangi <small_musa@hotmail.com>
2020-03-25 20:37:30 +0100

    updated tests to check for proper replacement of literals

--- spec/text-buffer-spec.js
+++ spec/text-buffer-spec.js
@@ -2,9 +2,10 @@ const path = require('path')
 const TextBuffer = require('../src/text-buffer')
 
 describe('when a buffer is already open', () => {
+  const filePath = path.join(__dirname, 'fixtures', 'sample.js')
+  const buffer = new TextBuffer()
+
   it('replaces foo( with bar( using /\bfoo\\(\b/gim', () => {
-    const filePath = path.join(__dirname, 'fixtures', 'sample.js')
-    const buffer = new TextBuffer()
     buffer.setPath(filePath)
     buffer.setText('foo(x)')
     buffer.replace(/\bfoo\(\b/gim, 'bar(')
@@ -12,13 +13,20 @@ describe('when a buffer is already open', () => {
     expect(buffer.getText()).toBe('bar(x)')
   })
 
-  it('replaces tstat_fvars()->curr_setpoint[HEAT_EN] = new_tptr->heat_limit; with tstat_set_curr_setpoint(HEAT_EN, new_tptr->heat_limit);', () => {
-    const filePath = path.join(__dirname, 'fixtures', 'sample.js')
-    const buffer = new TextBuffer()
-    buffer.setPath(filePath)
-    buffer.setText('if (tstat_fvars()->curr_setpoint[HEAT_EN] > new_tptr->heat_limit) { tstat_fvars()->curr_setpoint[HEAT_EN] = new_tptr->heat_limit; }')
-    buffer.replace(/tstat_fvars\(\)->curr_setpoint\[(.+?)\] = (.+?);/, 'tstat_set_curr_setpoint($1, $2);')
+  describe('should properly replace regex literals', () => {
+    it('replaces tstat_fvars()->curr_setpoint[HEAT_EN] = new_tptr->heat_limit; with tstat_set_curr_setpoint(HEAT_EN, new_tptr->heat_limit);', () => {
+      buffer.setPath(filePath)
+      buffer.setText('tstat_fvars()->curr_setpoint[HEAT_EN] = new_tptr->heat_limit;')
+      buffer.replace(/tstat_fvars\(\)->curr_setpoint\[(.+?)\] = (.+?);/, 'tstat_set_curr_setpoint($1, $2);')
 
-    expect(buffer.getText()).toBe('if (tstat_fvars()->curr_setpoint[HEAT_EN] > new_tptr->heat_limit) { tstat_set_curr_setpoint(HEAT_EN, new_tptr->heat_limit); }')
+      expect(buffer.getText()).toBe('tstat_set_curr_setpoint(HEAT_EN, new_tptr->heat_limit);')
+    })
+
+    it('replaces atom/flight-manual.atom.io with atom/flight-manualatomio', () => {
+      buffer.setText('atom/flight-manual.atom.io')
+      buffer.replace(/\.(atom)\./, '$1')
+
+      expect(buffer.getText()).toBe('atom/flight-manualatomio')
+    })
   })
 })
